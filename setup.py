0bf4561de069f0df8d3d339e70f79878d19d8bd5
Suraj Yerramilli <surajyerramilli@gmail.com>
2025-10-26 19:57:11 -0400

    minor updates to setup

--- setup.py
+++ setup.py
@@ -1,25 +1,59 @@
 # Initialize setup
 import os
 import sys
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
+
 here = os.path.abspath(os.path.dirname(__file__))
 
+# Read requirements
 with open('requirements.txt') as fh:
-    requirements = [line.strip() for line in fh.readlines()]
+    requirements = [line.strip() for line in fh.readlines() if line.strip() and not line.startswith('#')]
 
 with open('exp_requirements.txt') as f:
-      exp_requirements = f.read().splitlines()
+    exp_requirements = [line.strip() for line in f.readlines() if line.strip() and not line.startswith('#')]
 
-setup(name='lvgp-bayes',
-      version='0.3.0',
-      description='LVGP models with bayesian inference',
-      url='http://github.com/syerramilli/lvgp-bayes',
-      author='Suraj Yerramilli',
-      author_email='surajyerramilli@gmail.com',
-      license='MIT',
-      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "experiments/*", "notebooks"]),
-      install_requires=requirements,
-      extras_require={
-          'experiments': exp_requirements
-      },
-      zip_safe=False)
+# Read long description from README
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='lvgp-bayes',
+    version='0.3.0',
+    description='Latent Variable Gaussian Process models with fully Bayesian inference',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/syerramilli/lvgp-bayes',
+    author='Suraj Yerramilli',
+    author_email='surajyerramilli@gmail.com',
+    license='Proprietary - Academic and Non-Commercial Research Use Only',
+
+    # Python version requirement
+    python_requires='>=3.10',
+
+    # Package discovery
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "experiments", "notebooks"]),
+
+    # Dependencies
+    install_requires=requirements,
+    extras_require={
+        'experiments': exp_requirements,
+    },
+
+    # Classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+
+    # Keywords
+    keywords='gaussian-processes bayesian-inference latent-variables machine-learning',
+
+    zip_safe=False
+)
